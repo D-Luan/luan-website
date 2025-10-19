8711fae5678a3e9c8e412334d329838151846400
d-luan <dluan1285@gmail.com>
2025-10-19 14:03:53 -0300

    feat(backend): add Article and Project models

---
+++ backend/src/LuanWebsite.WebAPI/Models/Article.cs
@@ -0,0 +1,16 @@
+﻿namespace LuanWebsite.WebAPI.Models;
+
+public class Article
+{
+    public int Id { get; set; }
+    
+    public string Title { get; set; } = string.Empty;
+
+    public string Content { get; set; } = string.Empty;
+
+    public string Category { get; set; } = string.Empty;
+
+    public DateTime CreatedDate { get; set; }
+
+    public string? ProjectLink { get; set; }
+}
---
+++ backend/src/LuanWebsite.WebAPI/Models/Project.cs
@@ -0,0 +1,18 @@
+﻿namespace LuanWebsite.WebAPI.Models;
+
+public class Project
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Content { get; set; } = string.Empty;
+
+    public string Category { get; set; } = string.Empty;
+
+    public DateTime CreatedDate { get; set; }
+
+    public string ProjectLink { get; set; } = string.Empty;
+
+    public string? ArticleLink { get; set; }
+}
