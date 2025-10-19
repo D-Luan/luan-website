c33f919f9f53f01686f403cfa4f2e144a3f7981c
d-luan <dluan1285@gmail.com>
2025-10-19 14:06:06 -0300

    feat(backend): add DbContext and Migrations to the project

---
+++ backend/src/LuanWebsite.WebAPI/Data/WebsiteContext.cs
@@ -0,0 +1,16 @@
+﻿using LuanWebsite.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LuanWebsite.WebAPI.Data;
+
+public class WebsiteContext : DbContext
+{
+    public WebsiteContext(DbContextOptions<WebsiteContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Article> Articles { get; set; }
+
+    public DbSet<Project> Projects { get; set; }
+}
---
+++ backend/src/LuanWebsite.WebAPI/Migrations/20251019135937_InitialCreate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LuanWebsite.WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Content = table.Column<string>(type: "TEXT", nullable: false),
+                    Category = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ProjectLink = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Content = table.Column<string>(type: "TEXT", nullable: false),
+                    Category = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ProjectLink = table.Column<string>(type: "TEXT", nullable: false),
+                    ArticleLink = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articles");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
